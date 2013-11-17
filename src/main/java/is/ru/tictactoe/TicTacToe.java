e91e84f02568c1a5066d5adbf646114e20d6c722
magnusolafur <ljosastaur@gmail.com>
2013-11-17 19:41:55 +0000

    Added the TicTacToe class

---
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -0,0 +1,19 @@
+package is.ru.tictactoe;
+
+import static spark.Spark.*;
+import spark.*;
+
+public class TicTacToe {
+
+   public static void main(String[] args) {
+      
+      get(new Route("/hello") {
+         @Override
+         public Object handle(Request request, Response response) {
+            return "Hello World!";
+         }
+      });
+
+   }
+
+}
