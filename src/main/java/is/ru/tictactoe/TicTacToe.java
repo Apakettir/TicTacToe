696842fba09d795c167bda3053585b7fdc905cea
Keli <thorkell@me.com>
2013-11-18 17:40:15 +0000

    resolved more conflicts:keli

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -8,11 +8,9 @@ public class TicTacToe {
    public static void main(String[] args) {
 
       staticFileLocation("/public");
-      get(new Route("/hello") {
-
-   		setPort(Integer.valueOf(System.getenv("PORT")));
+      setPort(Integer.valueOf(System.getenv("PORT")));
       
-      get(new Route("/") {
+      get(new Route("/hello") {
          @Override
          public Object handle(Request request, Response response) {
             return 0;
