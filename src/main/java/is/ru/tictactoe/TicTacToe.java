10cd9ce8990b1874728986ee969c628ce1a23882
magnusolafur <ljosastaur@gmail.com>
2013-11-20 15:55:23 +0000

    Set up the API, and implemented the test for the toString() and the method itself

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -1,22 +1,53 @@
 package is.ru.tictactoe;
 
-import static spark.Spark.*;
-import spark.*;
-
 public class TicTacToe {
+    private char board[][];
 
-   public static void main(String[] args) {
+    public static final int n=3;
+    public static final int m=3;
 
-      staticFileLocation("/public");
-      setPort(Integer.valueOf(System.getenv("PORT")));
-      
-      get(new Route("/hello") {
-         @Override
-         public Object handle(Request request, Response response) {
-            return 0;
-         }
-      });
+    public TicTacToe(){
+        board = new char[n][m];
+        for(int i=0; i<board.length; i++)
+        {
+            for (int j=0; j<board.length; j++)
+            {
+                board[i][j]='-';
+            }
+        }
+    }
 
-   }
+    public String toString(){
+        String str = "";
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board.length; j++){
+                str += board[i][j] + " ";
+            }
+            str += "\n";
+        }
+        return str;
+    }
+
+    private void markX(String coords){
+
+    }
+
+    private void markO(String coords){
+
+    }
+
+    private void checkWin(){
+
+    }
+
+    private int convertToArray(String coords){
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+
+    }
 
 }
--- src/test/java/is/ru/tictactoe/TicTacToeTest.java
+++ src/test/java/is/ru/tictactoe/TicTacToeTest.java
@@ -1,12 +1,24 @@
 package is.ru.tictactoe;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
 
 /**
  * Unit test for simple App.
  */
 public class TicTacToeTest {
+
+	@Test
+    public void testPrint() {
+    	TicTacToe game = new TicTacToe();
+    	assertEquals("- - - \n- - - \n- - - \n",game.toString());
+
+    }
+
 
 }
