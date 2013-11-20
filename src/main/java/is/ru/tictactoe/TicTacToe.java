47a6ce733226311195877c509c9b808361b62857
magnusolafur <ljosastaur@gmail.com>
2013-11-20 16:25:32 +0000

    Implementented the Pair class and convertToArrayCoords method and the helper function

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -6,6 +6,15 @@ public class TicTacToe {
     public static final int n=3;
     public static final int m=3;
 
+    private class pair{
+        private int x;
+        private int y;
+        public pair(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+
     public TicTacToe(){
         board = new char[n][m];
         for(int i=0; i<board.length; i++)
@@ -28,11 +37,11 @@ public class TicTacToe {
         return str;
     }
 
-    private void markX(String coords){
+    public void markX(String coords){
 
     }
 
-    private void markO(String coords){
+    public void markO(String coords){
 
     }
 
@@ -40,9 +49,20 @@ public class TicTacToe {
 
     }
 
-    private int convertToArray(String coords){
 
-        return 0;
+    private pair convertToArrayCoords(String coords){
+        char[] cc = coords.toCharArray();
+        int x = Integer.parseInt(cc[0]) - 1;
+        int y = charToInt(cc[1]);
+        return new Pair(x, y);
+    }
+
+    //helper function for the convertToArrayCoords method to convert char to the correct integer
+    private int charToInt(char c){
+        if(c == 'a') return 0;
+        else if(c == 'b') return 1;
+        else if (c == 'c') return 2;
+        else throw new IllegalArgumentException("Illegal character");
     }
 
     public static void main(String[] args) {
