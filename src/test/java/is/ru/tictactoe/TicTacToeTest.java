4ecfa8a87fc33148d730a491b0df0dc558dfc30f
FreyrGudna <freyr123@gmail.com>
2013-11-21 16:32:52 +0000

    Check win komid

--- src/test/java/is/ru/tictactoe/TicTacToeTest.java
+++ src/test/java/is/ru/tictactoe/TicTacToeTest.java
@@ -31,7 +31,6 @@ public class TicTacToeTest {
     	game.markO("c2");
     	assertEquals("- - - \n- - - \n- O - \n",game.toString());
     }    
-/*
     @Test
     public void testCheckWin(){
         TicTacToe game = new TicTacToe();
@@ -81,5 +80,5 @@ public class TicTacToeTest {
         boolean jamm = false;
         assertEquals(jamm, game.checkWin());
 
-    }*/
+    }
 }
